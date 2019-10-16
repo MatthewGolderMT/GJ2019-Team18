960a56c847053f2ed63e7f5cb09ed3222c74ac86
Liz Wright <lizzy.j.wright@gmail.com>
2019-10-16 16:00:15 +0100

    Adding 2 players to the scene. Adding a fix for all players moving at once

--- Assets/Square.cs
+++ Assets/Square.cs
@@ -9,16 +9,19 @@ public class Square : MonoBehaviour
 	public float RotationSpeed = 100f;
 	private PlayerControls _controls;
 	private Vector2 _move;
-	//private Vector2 _rotate;
 	private Vector2 _lookDirection;
 
 	private void Awake()
 	{
 		_controls = new PlayerControls();
+
+		//player input can handle the device assignment, but we need to set it to match our control scheme
+		PlayerInput playerInput = GetComponent<PlayerInput>();
+		_controls.devices = playerInput.devices;
+
 		_controls.Gameplay.Grow.performed += ctx => Grow();
 		_controls.Gameplay.Move.performed += ctx => _move = ctx.ReadValue<Vector2>();
 		_controls.Gameplay.Move.canceled += ctx => _move = Vector2.zero;
-		//_controls.Gameplay.Rotation.performed += ctx => _rotate = ctx.ReadValue<Vector2>();
 		_controls.Gameplay.Rotation.performed += ctx => _lookDirection = ctx.ReadValue<Vector2>();
 		_controls.Gameplay.Rotation.canceled += ctx => _lookDirection = Vector2.zero;
 	}
