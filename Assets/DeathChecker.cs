31cd22fb6fe3309a73f991b75dcdeb1e2d8f242e
Liz Wright <lizzy.j.wright@gmail.com>
2019-10-18 17:58:55 +0100

    adding death state

---
+++ Assets/DeathChecker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathChecker : MonoBehaviour
+{
+	public List<Player> players;
+
+
+	private void Update()
+	{
+		int deadCount = 0;
+		foreach (Player player in players)
+		{
+			if (player.currentHealth <= 0)
+			{
+				deadCount++;
+			}
+		}
+		if (deadCount == 3)
+		{
+			SceneManager.LoadScene("TitleScreen");
+
+		}
+	}
+}
