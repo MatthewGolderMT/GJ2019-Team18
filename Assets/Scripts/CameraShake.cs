9455ce65d438d1d294efb7953664626354dfa0dd
Liz Wright <lizzy.j.wright@gmail.com>
2019-10-18 17:47:55 +0100

    adding in screen shake on damage

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -6,41 +6,70 @@ public class CameraShake : MonoBehaviour
 	// Transform of the camera to shake. Grabs the gameObject's transform
 	// if null.
 	public Transform camTransform;
-	
+
 	// How long the object should shake for.
-	public float shakeDuration = 0f;
-	
+	public float MaxShakeTime = 0.5f;
+	private float _timeShaking = 0;
+	private bool _isShaking = false;
 	// Amplitude of the shake. A larger value shakes the camera harder.
 	public float shakeAmount = 0.7f;
 	public float decreaseFactor = 1.0f;
-	
+
 	Vector3 originalPos;
-	
+
 	void Awake()
 	{
 		if (camTransform == null)
 		{
 			camTransform = GetComponent(typeof(Transform)) as Transform;
 		}
+		_timeShaking = 0;
 	}
-	
+
 	void OnEnable()
 	{
 		originalPos = camTransform.localPosition;
+		MechController.damageEvent += ShakeCamera;
+	}
+
+	private void OnDisable()
+	{
+		MechController.damageEvent -= ShakeCamera;
+
+	}
+
+	public void ShakeCamera()
+	{
+		_isShaking = true;
 	}
 
 	void Update()
 	{
-		if (shakeDuration > 0)
+		if (_isShaking)
+		{
+			if (_timeShaking < MaxShakeTime)
+			{
+				camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
+				_timeShaking += Time.deltaTime * decreaseFactor;
+			}
+			else
+			{
+				_isShaking = false;
+				_timeShaking = 0;
+			}
+		}
+		/*
+		if (MaxShakeTime > 0)
 		{
 			camTransform.localPosition = originalPos + Random.insideUnitSphere * shakeAmount;
-			
-			shakeDuration -= Time.deltaTime * decreaseFactor;
+
+			MaxShakeTime -= Time.deltaTime * decreaseFactor;
 		}
 		else
 		{
-			shakeDuration = 0f;
+			MaxShakeTime = 0f;
 			camTransform.localPosition = originalPos;
 		}
+		*/
 	}
 }
